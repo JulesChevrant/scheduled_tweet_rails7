c14ec906e33b88c5f6d7cf52487bc7dc52936918
jules chevrant breton <jules.chevrant@hotmail.fr>
2022-03-09 19:25:47 +0100

    created registration routes and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create" # A same URL (/sign_up) can handle multiple types of requests (verbs)
 
   root to: "main#index"
 end
