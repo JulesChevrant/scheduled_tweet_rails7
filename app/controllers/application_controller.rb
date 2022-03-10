496c722fedee6cd57eb068262a834c7aa7c6a877
jules chevrant breton <jules.chevrant@hotmail.fr>
2022-03-10 19:46:37 +0100

    modif navbar for current.user state + css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # Everything defined here is shared in all controllers
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
 end
