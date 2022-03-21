ea75f7c3a62e7e55ab3d17dc89529c108d7e9242
jules chevrant breton <jules.chevrant@hotmail.fr>
2022-03-21 18:15:52 +0100

    created twitter account model, added validations and belongs to & added route for callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 # password_confirmation:string virtual
 
 class User < ApplicationRecord
+  has_many :twitter_accounts
   has_secure_password # method from rails : will convert the password, make sure the password and password_confirmation match. Will then run the pw through bcrypt to credit digest. That will convert them into a password_digest for us.
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
